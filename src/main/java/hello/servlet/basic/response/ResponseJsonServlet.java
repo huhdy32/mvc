047984d94e30617add8c4b50c2fd1473d5f59bf8
huhdy32 <mong3268@gmail.com>
2025-04-05 10:41:37 +0900

    level: HttpServletResponse를 활용하여 json 응답

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        HelloData helloData = new HelloData("name", 12);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PrintWriter writer = resp.getWriter();
+        writer.write(objectMapper.writeValueAsString(helloData));
+    }
+}
